5653464e0f6b558369f5e9d9af15b8e1d46d127a
Lam-Laptop <lam.tran@aalto.fi>
2022-08-03 15:55:59 +0300

    4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
     return(1)
 }
 
+const totalLikes = (blogs) => {
+    return(
+        blogs.map( blog => blog.likes)
+             .reduce( (x, y) => x + y, 0)
+    )
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
