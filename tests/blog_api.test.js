b4734acdcb10fe9a233a4eddc65dc249adf4fe4d
Lam-Laptop <lam.tran@aalto.fi>
2022-08-06 18:22:34 +0300

    4.9

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -8,17 +8,14 @@ const api = supertest(app)
 
 beforeEach(async () => {
     await Blog.deleteMany({})
-    console.log('cleared')
 
     const blogs = helper.initialBlogs
         .map( blog => new Blog(blog))
     const promiseArray = blogs.map( blog => blog.save())
     await Promise.all(promiseArray)
-    console.log('saved')
 })
 
 test('blogs returned as JSON', async () => {
-    console.log('entered test1')
     await api
         .get('/api/blogs')
         .expect(200)
@@ -26,17 +23,22 @@ test('blogs returned as JSON', async () => {
 }) 
 
 test('returned two blogs', async () => {
-    console.log('entered test2')
     const response = await api.get('/api/blogs')
 
     expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
 test('correct content', async () => {
-    console.log('entered test3')
     const response = await api.get('/api/blogs')
 
     expect(response.body[0].title).toBe("blog1")
+})
+
+test('every blog has unique id', async () => {
+    const response = await api.get('/api/blogs')
+    const blogs = response.body
+    
+    blogs.forEach( blog => expect(blog.id).toBeDefined())
 })
 
 afterAll(() => {
