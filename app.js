f7f25f1a59f1386ee1d7c6b75d3034b8c6f9e59a
Lam-Laptop <lam.tran@aalto.fi>
2022-09-05 21:35:45 +0300

    part5 e2e test

--- app.js
+++ app.js
@@ -31,6 +31,11 @@ app.use('/api/blogs', blogsRouter)
 app.use('/api/users', userRouter)
 app.use('/api/login', loginRouter)
 
+if (process.env.NODE_ENV === 'test') {
+    const testingRouter = require('./controllers/tests')
+    app.use('/api/testing', testingRouter)
+}
+
 app.use(middleware.unknownEndpont)
 app.use(middleware.errorHandler)
     
